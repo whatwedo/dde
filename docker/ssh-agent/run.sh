43e592ebc9fa42c8b30199598ff09945101f775f
Felix Imobersteg <felix@whatwedo.ch>
2018-10-19 15:05:18 +0200

    fix(ssh-agent): remove debug output

--- docker/ssh-agent/run.sh
+++ docker/ssh-agent/run.sh
@@ -3,8 +3,6 @@
 # Exit on error
 set -e
 
-env
-
 # Cleanup old sockets
 rm -f $SSH_AUTH_SOCK
 
@@ -14,7 +12,6 @@ useradd -d /home/dde -u $DDE_UID -g $DDE_GID -c "dde" -s /bin/bash -N -o -m dde
 
 # Create socket dir
 mkdir -p $SOCKET_DIR && chown -R dde:dde $SOCKET_DIR
-ls -la $SOCKET_DIR
 
 # Start ssh agent
 gosu dde sh -c "ssh-agent -a $SSH_AUTH_SOCK -D"
