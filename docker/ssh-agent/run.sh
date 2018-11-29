6bf29632b8cdc0a330df0468d23b001e87ca98c2
Lukas Lücke <lukas@luecke.me>
2018-11-29 11:09:34 +0100

    fix(ssh-agent): continue if group/user exists
    otherwise restarting the ssh-agent container would always fail

--- docker/ssh-agent/run.sh
+++ docker/ssh-agent/run.sh
@@ -7,8 +7,8 @@ set -e
 rm -f $SSH_AUTH_SOCK
 
 # Add dde user/group
-groupadd -g $DDE_GID -o dde
-useradd -d /home/dde -u $DDE_UID -g $DDE_GID -c "dde" -s /bin/bash -N -o -m dde
+groupadd -g $DDE_GID -o dde || true
+useradd -d /home/dde -u $DDE_UID -g $DDE_GID -c "dde" -s /bin/bash -N -o -m dde || true
 
 # Create socket dir
 mkdir -p $SOCKET_DIR && chown -R dde:dde $SOCKET_DIR
