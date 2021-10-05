db7ba292ac3a524324daaf0b58f781baa03d6412
Maurizio Monticelli <rugbymauri@gmail.com>
2021-10-05 09:59:40 +0200

    feat: sync mode detect function

--- dde.sh
+++ dde.sh
@@ -298,6 +298,30 @@ function project:status {
 	docker-compose ps
 }
 
+function _syncMode {
+    SYNC_MODE=no-sync
+    if [ -f ./mutagen.yml ]; then
+        SYNC_MODE=mutagen
+    fi
+
+    if [ -f ./docker-sync.yml ]; then
+        SYNC_MODE=docker-sync
+    fi
+
+    if [ -f ./docker-compose.override.yml ]; then
+        if [ $(grep "^x-dde:" docker-compose.override.yml | cut -d':' -f2) ]; then
+            SYNC_MODE=$(grep "x-dde:" docker-compose.override.yml | cut -d':' -f2 | xargs)
+        fi
+    fi
+
+    if [[ "${SYNC_MODE}" = "no-sync" ]]  || [[ "${SYNC_MODE}" = "docker-sync" ]] || [[ "${SYNC_MODE}" = "mutagen" ]]; then
+        _logYellow "Sync mode is ${SYNC_MODE}"
+    else
+        _logRed "Unknown Sync mode ${SYNC_MODE}"
+        exit 1
+    fi
+}
+
 
 function _checkProject {
     if [ "${ROOT_DIR}" == "$(pwd)" ]; then
