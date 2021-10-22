211690e0f8730d2ace42f2421be5d976e92c7938
Maurizio Monticelli <rugbymauri@gmail.com>
2021-10-22 10:54:10 +0200

    feat: openUrl as internal function

--- commands/project/up.sh
+++ commands/project/up.sh
@@ -31,10 +31,7 @@ function project:up() {
     fi
     _logGreen "Finished startup successfully"
 
-    for vhost in $(grep VIRTUAL_HOST= docker-compose.yml | cut -d'=' -f2); do
-        _logYellow "visit: https://${vhost}"
-    done
-
+    _openUrl
 }
 
 function p:u() {
