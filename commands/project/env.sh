4ed45d454a4df9b30edd6c9e7c15dedae3921715
Maurizio Monticelli <rugbymauri@gmail.com>
2021-11-04 11:12:33 +0100

    feat: project:env show system:env also

--- commands/project/env.sh
+++ commands/project/env.sh
@@ -6,13 +6,21 @@
 
 function project:env() {
     _checkProject
+    local oldPwd=$(pwd)
     system:env
+    cd ${oldPwd}
 
+    echo ""
     _logGreen "DDE Project env:"
     if [ -f ./docker-compose.override.yml ]; then
         echo ./docker-compose.override.yml defined
     fi
     echo SYNC_MODE=${SYNC_MODE}
+
+
+    echo ""
+    _logYellow "Project services"
+    docker-compose ps --services
 
 }
 
