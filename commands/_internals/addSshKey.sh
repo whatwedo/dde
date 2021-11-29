0806d7f66e15d85d4ed0e47c0f077d08a40f98d3
Maurizio Monticelli <rugbymauri@gmail.com>
2021-11-29 13:43:02 +0100

    feat: check if ssh-key exists

--- commands/_internals/addSshKey.sh
+++ commands/_internals/addSshKey.sh
@@ -1,6 +1,20 @@
 
 function _addSshKey() {
     _logYellow "Adding SSH key (maybe passphrase required)"
+
+    return
+    local cmd="ls -A ~/.ssh 2>/dev/null"
+    if [ -n $(${cmd}) ]
+    then
+        _logGreen "ssh key ok"
+    else
+        _logRed "add some ssh keys use ssh-keygen"
+        exit 1
+    fi
+
     cd ${ROOT_DIR}
     docker-compose exec ssh-agent sh -c /import-keys.sh
 }
+
+
+
