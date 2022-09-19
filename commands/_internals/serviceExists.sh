637256ca0dffc9103f7b0b53da99e828d4877944
Mauri <rugbymauri@gmail.com>
2022-09-19 22:57:22 +0200

    fix: replace docker-compose by variable

--- commands/_internals/serviceExists.sh
+++ commands/_internals/serviceExists.sh
@@ -1,6 +1,6 @@
 _serviceExists() {
     local inArrayFound=1
-    for service in $(docker-compose ps --services)
+    for service in $(${DOCKER_COMPOSE} ps --services)
     do
         if [[ "${1}" == "${service}" ]]; then
             inArrayFound=0
