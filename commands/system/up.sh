c09196f6b85322297e5468accc67eb23df30b103
Mauri <rugbymauri@gmail.com>
2023-01-25 13:16:49 +0100

    feat: update command for all services

--- commands/system/up.sh
+++ commands/system/up.sh
@@ -7,12 +7,7 @@
 function system:up() {
     _ddeCheckUpdate
 
-    _logYellow "Creating network if required"
-    local networks=$(docker network inspect dde)
-    if [ "$(docker network ls --filter=name=${NETWORK_NAME} -q)" == "" ]; then
-        _logGreen "Create network ${NETWORK_NAME}"
-        docker network create ${NETWORK_NAME}
-    fi
+    _ddeCheckNetwork
 
     _logYellow "Creating default docker config.json"
     if [ ! -f ~/.docker/config.json ]; then
