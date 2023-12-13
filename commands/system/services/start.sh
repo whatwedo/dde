a8450a05346591c3e0a3d66075a18a2d7379adc7
Maurizio Monticelli <maurizio@whatwedo.ch>
2023-12-13 14:58:29 +0100

    feat: add start, stop, update command for services

---
+++ commands/system/services/start.sh
@@ -0,0 +1,30 @@
+## Start dde system service
+#
+# Command
+#    system:services:start <service>
+
+function system:services:enable() {
+    cd ${ROOT_DIR}
+
+    if [ -d "services/${1}" ]; then
+        _logGreen "System service: ${1} found"
+    else
+        _logRed "System service: :${1}: not found"
+        return
+    fi
+
+    _logYellow "Enable System services: ${1}"
+    cd services
+
+    for f in *; do
+    if [ -d "$f" ]; then
+        # Will not run if no directories are available
+        if [ -f "${f}/docker-compose.yml" ]; then
+            if [ "$f" == "${1}" ]; then
+                cd ${f}
+                ${DOCKER_COMPOSE} up -d
+            fi
+        fi
+    fi
+done
+}
