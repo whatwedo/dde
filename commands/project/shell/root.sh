ac5ebb05720be9d504512c46e324126ac8b9f7be
Simon Baerlocher <s.baerlocher@sbaerlocher.ch>
2024-02-22 15:09:18 +0100

    feat: move exec to shell

---
+++ commands/project/shell/root.sh
@@ -0,0 +1,36 @@
+## Opens privileged shell on first container
+#
+# Command
+#    project:shell:root
+#    s:e:r
+#    shell-root
+
+function project:shell:root() {
+    _checkProject
+    local service=$(docker run --rm -v $(pwd):/workdir mikefarah/yq:3 yq r --printMode p docker-compose.yml 'services.*' | head -n1 | sed 's/.*\.//')
+
+    if [[ "${1}" != "" ]]; then
+        if _serviceExists ${1}; then
+            service=${1}
+        else
+            _logRed "service ${1} does not exists in project"
+            _existingServices
+            return 1
+        fi
+    fi
+
+    if ! _serviceIsRunning ${service}; then
+        _logYellow "Project is not running. Starting ..."
+        project:up
+    fi
+
+    ${DOCKER_COMPOSE} exec ${service} /bin/sh -c "sh"
+}
+
+function p:e:r() {
+    project:shell:root
+}
+
+function shell-root() {
+    project:shell:root
+}
