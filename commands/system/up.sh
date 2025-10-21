44ac51c6ecfc06ec1e07d76f028093b579ace397
René Stalder <rene@whatwedo.ch>
2025-10-21 13:49:31 +0200

    refactor(commands.system.up): externalize network check and creation
    Move the network existence check and creation to an internal function to
    allow reusing it in other commands.
    
    Signed-off-by: René Stalder <rene@whatwedo.ch>

--- commands/system/up.sh
+++ commands/system/up.sh
@@ -7,11 +7,7 @@
 function system:up() {
     _ddeCheckUpdate
 
-    _logYellow "Creating network if required"
-    if [ "$(${DOCKER_BIN} network ls --filter=name=${NETWORK_NAME} -q)" == "" ]; then
-        _logGreen "Create network ${NETWORK_NAME}"
-        ${DOCKER_BIN} network create ${NETWORK_NAME}
-    fi
+    _ensureNetwork
 
     _logYellow "Creating default docker config.json"
     if [ ! -f ~/.docker/config.json ]; then
