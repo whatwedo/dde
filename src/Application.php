46afe3ea377bbd3c688ca7f18bfb97cfb5d835ce
Ueli Banholzer <ueli@whatwedo.ch>
2026-05-13 12:17:52 +0200

    chore(release): bump version to v2.0.0-beta.1
    Signed-off-by: Ueli Banholzer <ueli@whatwedo.ch>

--- src/Application.php
+++ src/Application.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpKernel\KernelInterface;
 
 final class Application extends BaseApplication
 {
-    public const string APP_VERSION = 'v2.0.0-alpha.5';
+    public const string APP_VERSION = 'v2.0.0-beta.1';
 
     private const array ALLOWED_COMMANDS = ['about', 'completion', 'help', 'list'];
 
